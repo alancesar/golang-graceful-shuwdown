c09acc2e73ce244d28ba772efc3df3d8b5410aba
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2023-02-03 18:51:23 -0300

    feat: replace gin by chi

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"context"
 	"github.com/alancesar/graceful-shuwdown-sample/app"
 	"github.com/alancesar/graceful-shuwdown-sample/server"
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	engine := gin.Default()
-	engine.Handle(http.MethodGet, "/", func(c *gin.Context) {
+	mux := chi.NewMux()
+	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("this request will finish even you press ctrl+c")
 		time.Sleep(5 * time.Second)
-		c.String(http.StatusOK, "finished")
+		_, _ = w.Write([]byte("finished"))
 	})
 
-	s := server.New(engine, ":8080")
+	s := server.New(mux, ":8080")
 	a := app.New(s)
 	a.Start(context.Background())
 }
