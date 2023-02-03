b160192afa38607dd177facee77531f034a64723
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2023-02-03 18:56:29 -0300

    refactor: place all code in main.go

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"context"
-	"github.com/alancesar/graceful-shuwdown-sample/app"
-	"github.com/alancesar/graceful-shuwdown-sample/server"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	ctx := context.Background()
+
 	mux := chi.NewMux()
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("this request will finish even you press ctrl+c")
@@ -18,7 +21,33 @@ func main() {
 		_, _ = w.Write([]byte("finished"))
 	})
 
-	s := server.New(mux, ":8080")
-	a := app.New(s)
-	a.Start(context.Background())
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Println(err)
+		}
+	}()
+
+	log.Println("all systems go!")
+
+	<-ctx.Done()
+	stop()
+
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("oops...")
+	}
+
+	fmt.Println("good bye")
 }
